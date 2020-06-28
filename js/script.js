2361a5a9a0063bc8173a57b5049ccd67d06600a5
John Mistica <47683574+johndev4@users.noreply.github.com>
2020-06-28 20:26:08 +0800

    Update files

--- js/script.js
+++ js/script.js
@@ -5,24 +5,15 @@ $(document).ready(function() {
     });
 
     $.ajax({
-        url: 'js/image1.txt',
+        url: 'js/hp-bf064tx.txt',
         type: 'get',
         success: function(result) {
             $('.img-container').html(result);
-            $('#img-components').on('load', function() {
-                $.ajax({
-                    url: 'js/image2.txt',
-                    type: 'get',
-                    success: function(result) {
-                        $('.img-container').html($('.img-container').html() + result);
-                        $('#img-cover').on('load', function() {
-                            $('.img-container').animate({
-                                left: '10%',
-                                opacity: '1'
-                            }, 1800);
-                        });
-                    }
-                });
+            $('#img-cover').on('load', function() {
+                $('.img-container').animate({
+                    left: '10%',
+                    opacity: '1.0'
+                }, 1800);
             });
         }
     });
